62da196c790a422a1bcd576b1262a4144751f504
Harshaan <bawaharshaan1610@gmail.com>
2025-03-18 20:46:11 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,31 +1,19 @@
-// Your code here...
-#include <stdio.h>
-
-void leftRotate(int arr[], int size, int rotations) {
-    for (int r = 0; r < rotations; r++) {
-        int temp = arr[0]; // Store the first element
-        for (int i = 0; i < size - 1; i++) {
-            arr[i] = arr[i + 1]; // Shift left
-        }
-        arr[size - 1] = temp; // Move first element to the end
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    int brr[N];
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
     }
-}
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    int k;
+    scanf("%d",&k);
+    for(int i=0;i<N;i++){
+        brr[i]=arr[(i+k)%N];
     }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    
-    int rotations = 2; // Number of left rotations
-    leftRotate(arr, size, rotations);
-    
-    printArray(arr, size); // Output: 3 4 5 1 2
-
+    for(int i=0;i<N;i++){
+        printf("%d\n",arr[i]);
+    }
     return 0;
 }
